54f9a7931c3ed037979e4fd65d56ed9db8d27993
AndryAmagua <andryamagua5@gmail.com>
2022-05-19 17:04:07 -0500

    modal

--- app/Views/blackjack/vistabj.php
+++ app/Views/blackjack/vistabj.php
@@ -6,10 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BlackJack</title>
-    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
-<<<<<<< HEAD
 
     <style>
         /* The Modal (background) */
@@ -61,13 +58,10 @@
         }
     </style>
 
-=======
-    
->>>>>>> 0cf87524472781b1b8f28614704f08f241f4f71d
+
 </head>
 
 <body>
-<<<<<<< HEAD
 
     <button id="myBtn">Open Modal</button>
     <!-- The Modal -->
@@ -79,33 +73,6 @@
             <p>Some text in the Modal..</p>
         </div>
     </div>
-
-=======
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-link" data-toggle="modal" data-target="#exampleModalCenter">
-    Información
-    </button>
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Información</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <h3 class="text-center">Reglas y Monedas</h3>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam eaque nulla veniam, nostrum sed voluptas itaque ducimus quia perferendis labore ad iste cumque quo voluptatibus ipsa quasi eveniet hic assumenda praesentium error repellat recusandae dolorum enim ut? Tempore, quasi cum ipsum eum beatae commodi ut expedita totam nostrum itaque odit?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
-            </div>
-        </div>
-    </div>
->>>>>>> 0cf87524472781b1b8f28614704f08f241f4f71d
     <h1>BLACKJACK</h1>
 
     <div class="dealer">
@@ -179,7 +146,6 @@
             <h4>Jugador 2: Chasi</h4>
         </div>
     </div>
-<<<<<<< HEAD
 
     <script>
         // Get the modal
@@ -208,11 +174,6 @@
             }
         }
     </script>
-=======
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
->>>>>>> 0cf87524472781b1b8f28614704f08f241f4f71d
 </body>
 
 </html>
