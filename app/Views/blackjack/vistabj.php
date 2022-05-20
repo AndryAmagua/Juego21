1038d08736add68606baec9f887993bf7276a4b9
e4ss1 <andreszumarraga21@hotmail.com>
2022-05-19 19:01:57 -0500

    cambios

--- app/Views/blackjack/vistabj.php
+++ app/Views/blackjack/vistabj.php
@@ -7,74 +7,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BlackJack</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
-
-    <style>
-        /* The Modal (background) */
-        .modal {
-            display: none;
-            /* Hidden by default */
-            position: fixed;
-            /* Stay in place */
-            z-index: 1;
-            /* Sit on top */
-            padding-top: 100px;
-            /* Location of the box */
-            left: 0;
-            top: 0;
-            width: 100%;
-            /* Full width */
-            height: 100%;
-            /* Full height */
-            overflow: auto;
-            /* Enable scroll if needed */
-            background-color: rgb(0, 0, 0);
-            /* Fallback color */
-            background-color: rgba(0, 0, 0, 0.4);
-            /* Black w/ opacity */
-        }
-
-        /* Modal Content */
-        .modal-content {
-            background-color: #b5ef55;
-            margin: auto;
-            padding: 20px;
-            border: 1px solid #888;
-            width: 80%;
-        }
-
-        /* The Close Button */
-        .close {
-            color: #aaaaaa;
-            float: right;
-            font-size: 28px;
-            font-weight: bold;
-        }
-
-        .close:hover,
-        .close:focus {
-            color: #000;
-            text-decoration: none;
-            cursor: pointer;
-        }
-    </style>
-
-
 </head>
 
 <body>
 
-    <button id="myBtn">Open Modal</button>
+    <button id="myBtn" class="btn">Informacion y Monedas</button>
     <!-- The Modal -->
     <div id="myModal" class="modal">
-
         <!-- Modal content -->
         <div class="modal-content">
             <span class="close">&times;</span>
-            <p>Some text in the Modal..</p>
+            <h2>Informacion y Monedas</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus dicta consequuntur sunt. Placeat error, incidunt omnis corporis recusandae rem cum sint consequuntur eaque et quam tempora voluptates perferendis odio amet?</p>
         </div>
     </div>
     <h1>BLACKJACK</h1>
-
     <div class="dealer">
         <img src="<?php echo base_url(); ?>/assets/images/BACK.png" alt="">
         <img src="<?php echo base_url(); ?>/assets/images/2-C.png" alt="">
@@ -146,34 +93,6 @@
             <h4>Jugador 2: Chasi</h4>
         </div>
     </div>
-
-    <script>
-        // Get the modal
-        var modal = document.getElementById("myModal");
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    </script>
+    <script src="<?php echo base_url(); ?>/assets/js/script.js"></script>
 </body>
-
 </html>
